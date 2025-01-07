b58174b5672bb3fb86a2d95c1e30ea1506a324aa
anupsv <anupsv@users.noreply.github.com>
2025-01-07 20:29:40 +0530

    removing unwanted const

--- src/consts.rs
+++ src/consts.rs
@@ -3,7 +3,6 @@ pub const SIZE_OF_G1_AFFINE_COMPRESSED: usize = 32; // in bytes
 pub const SIZE_OF_G2_AFFINE_COMPRESSED: usize = 64; // in bytes
 
 pub const FIAT_SHAMIR_PROTOCOL_DOMAIN: &[u8] = b"EIGENDA_FSBLOBVERIFY_V1_"; // Adapted from 4844
-pub const FIELD_ELEMENTS_PER_BLOB: u64 = 131072; // 32MB max blob size
 pub const KZG_ENDIANNESS: Endianness = Endianness::Big; // Choose between Big or Little.
 
 pub const RANDOM_CHALLENGE_KZG_BATCH_DOMAIN: &[u8] = b"EIGENDA_RCKZGBATCH___V1_"; // Adapted from 4844
