fe7383a25ac9d3d8f058642e43c33ca24fd65b2d
anupsv <anupsv@users.noreply.github.com>
2025-01-12 09:19:56 +0530

    fixing endianess to big across functions

--- src/consts.rs
+++ src/consts.rs
@@ -7,11 +7,3 @@ pub const FIAT_SHAMIR_PROTOCOL_DOMAIN: &[u8] = b"EIGENDA_FSBLOBVERIFY_V1_"; // A
 
 /// Ref: https://github.com/ethereum/consensus-specs/blob/master/specs/deneb/polynomial-commitments.md#blob
 pub const RANDOM_CHALLENGE_KZG_BATCH_DOMAIN: &[u8] = b"EIGENDA_RCKZGBATCH___V1_"; // Adapted from 4844
-
-pub const KZG_ENDIANNESS: Endianness = Endianness::Big; // Choose between Big or Little.
-
-#[derive(Debug, Clone, Copy)]
-pub enum Endianness {
-    Big,
-    Little,
-}
